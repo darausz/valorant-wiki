99166b6870cd659850656354c2cf00e012289614
dareaus <darauszhang@gmail.com>
2024-09-16 14:39:18 -0400

    updated router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
+// import {BrowserRouter as Router} from "react-router-dom";
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
